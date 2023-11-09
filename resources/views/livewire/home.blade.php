ff76044182c9fb08387439cfd2cf27fd0c5f5793
KoZin <cuzinminthu@gmail.com>
2023-11-09 21:54:08 +0630

    add livewire route

---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <h1>This is home component.</h1>
+    <p>Name: {{ $name }}</p>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Post;
 use App\Http\Livewire\User;
+use App\Http\Livewire\Home;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,4 @@ Route::get('/', function () {
 
 Route::get('/post', Post::class);
 Route::get('/user', User::class);
+Route::get('/home/{name?}', Home::class);
